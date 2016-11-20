816c6d223ff71e873a1090f4aa0af2905eda69c9
khrist14n <khrist14n@gmail.com>
2016-11-20 17:31:38 -0500

    houses

--- shop_houses_html/js/content.js
+++ shop_houses_html/js/content.js
@@ -1,3 +1,28 @@
+function slider(){
+    $("#slider").load(
+        'tpl/content/home/slider.html',
+        function(){
+
+        }
+    );
+}
+
+
+function list(){
+    $("#list").load(
+        'tpl/content/home/list.html',
+        function(){
+
+        }
+    );
+}
+
+
 function home(){
-    $("#content").load('tpl/content/home.html');
+    $("#content").load(
+        'tpl/content/home.html',
+        function(){
+            slider();
+        }
+    );
 }
