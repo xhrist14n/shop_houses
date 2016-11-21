32f9b78e97f483cade0c69374c06d6e5064b6a56
khrist14n <khrist14n@gmail.com>
2016-11-20 19:06:20 -0500

    houses

--- shop_houses_html/js/menu.js
+++ shop_houses_html/js/menu.js
@@ -10,8 +10,5 @@ function menu(){
 function active_menu(id){
     $('#navbar-menu').find('li').removeClass('active');
     $('#navbar-right-menu').find('li').removeClass('active');
-
-    $('#navbar-menu').find('#'+id).addClass('active');
-
-    $('#navbar-right-menu').find('#'+id).addClass('active');
+    $('#'+id).addClass('active');
 }
